c1ad4bff9b5643e39c9982828b679baa60082bf8
chaabanoo <chaaban.barakat@outlook.com>
2024-03-06 12:56:11 +0100

    added a search functionality

--- src/main/java/org/workshop/coffee/repository/SearchRepository.java
+++ src/main/java/org/workshop/coffee/repository/SearchRepository.java
@@ -19,7 +19,13 @@ public class SearchRepository {
     DataSource dataSource;
 
     public List<Product> searchProduct (String input) {
-        return null;
+        //lowercase the input
+        var lowerInput = input.toLowerCase(Locale.ROOT);
+        //create a query that matches the input with the product description and product name
+        var query = em.createNativeQuery("Select * from Product where lower(description) like '%" + lowerInput + "%' OR lower(product_name) like '%" + lowerInput + "%'", Product.class);
+        //execute the query and return the result
+        var resultList = (List<Product>) query.getResultList();
+        return resultList;
     }
 
 }
